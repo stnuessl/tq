35e60a2fc3e976a8fb950f2705bae8b546b3e749
Steffen Nüssle <nuessle.steffen@gmail.com>
2018-11-11 14:29:50 +0100

    url-client: 	- Added license information at the top of the file (was previously 	forgotten). 	- Made the type content check for the HTTP-GET 	response ignore the case of the characters.

--- src/query/url-client.cpp
+++ src/query/url-client.cpp
@@ -1,6 +1,26 @@
+/*
+ * Copyright (C) 2014  Steffen Nüssle
+ * tq - Twitch Query
+ *
+ * This file is part of tq.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <utility>
 #include <stdexcept>
+#include <cctype>
 
 #include "url-client.hpp"
 
@@ -81,8 +101,11 @@ std::string url_client::get_response(const std::string &url)
  
         throw std::runtime_error(err);
     }
+
+    for (auto &x : _header)
+        x = std::tolower(x);
     
-    if (_header.find("Content-Type: application/json") == std::string::npos)
+    if (_header.find("content-type: application/json") == std::string::npos)
         throw std::runtime_error("Server sent invalid MIME type:\n" + _header);
         
     return std::move(_response);
